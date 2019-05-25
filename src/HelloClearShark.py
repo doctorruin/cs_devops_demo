cb4752e5970925f3ccb672bfd9802d8a849f1ab4
rabey <randolphzemail@gmail.com>
2019-05-25 16:56:13 -0700

    adding some comments

--- src/HelloClearShark.py
+++ src/HelloClearShark.py
@@ -1,26 +1,37 @@
 # Hello World example to implement DevOps tools
-#
+# author: Randolph Abeyta
+
 
 def print_string():
 
     """
+
     Print String to console
-    :return:
+
     """
 
+    # Retrieve string and place it in var
     message = clearshark_string()
 
+    # Prints string to console
     print(message)
 
 
 def clearshark_string():
 
     """
+
     Creates String and returns it
+
     :return:
+        String
+
     """
+
+    # Instantiate string variable
     message = "Hello, ClearShark!"
 
+    # Returns var message
     return message
 
 
